b333c93aec473b46b2fecfb56cea2785199660ba
4face-studi0 <4face.studio@gmail.com>
2019-09-06 11:11:25 +0800

    Better `Any.version` algorithm

--- dsl/src/main/kotlin/studio/forface/easygradle/dsl/extensions.kt
+++ dsl/src/main/kotlin/studio/forface/easygradle/dsl/extensions.kt
@@ -8,8 +8,19 @@ import org.gradle.kotlin.dsl.exclude
 /**
  * @return receiver string ( usually a dependency notation ) with version
  * E.g. `` `kotlin-jvm` version "1.3.50" ``
+ *
+ * @throws IllegalArgumentException if format is not correct.
  */
-infix fun Any.version(version: String) = "$this:$version"
+infix fun Any.version(version: String): String {
+    val string = this.toString()
+    val parts = string.split(':')
+
+    require((parts.size == 2 || parts.size == 3)) {
+        "Invalid dependency format: '$string'. Expected 'group:artifact' or 'group:artifact:version'"
+    }
+
+    return "${parts[0]}:${parts[1]}:$version"
+}
 
 // region exclude utils
 fun ModuleDependency.exclude(vararg any: Any) {
