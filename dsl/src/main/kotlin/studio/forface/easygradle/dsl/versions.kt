811cec72d80caef3a2470292344eeaa46537d1c8
4face-studi0 <4face.studio@gmail.com>
2019-09-06 12:50:27 +0800

    Fixed Kotlin version

--- dsl/src/main/kotlin/studio/forface/easygradle/dsl/versions.kt
+++ dsl/src/main/kotlin/studio/forface/easygradle/dsl/versions.kt
@@ -1,6 +1,6 @@
 package studio.forface.easygradle.dsl
 
-internal const val defaultKotlinVersion =               "Aug 22, 2019"      // Updated: Aug 22, 2019
+internal const val defaultKotlinVersion =               "1.3.50"            // Updated: Aug 22, 2019
 internal const val defaultCoroutinesVersion =           "1.3.1"             // Updated: Sep 05, 2019
 internal const val defaultSerializationVersion =        "0.12.0"            // Updated: Aug 23, 2019
 internal const val defaultKtorVersion =                 "1.2.4"             // Updated: Sep 02, 2019
