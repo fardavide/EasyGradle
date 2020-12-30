f5691490f15505f1339ca4e2a6799c831ff0bb86
4face-studi0 <4face.studio@gmail.com>
2020-12-30 15:12:03 +0100

    DSL 3.0

--- dsl/base/build.gradle.kts
+++ dsl/base/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
 object Plugin {
     const val id = "studio.forface.easygradle"
     const val name = "EasyGradle"
-    const val version = "2.8" // TODO project.version
+    const val version = "3.0" // TODO project.version
 }
 
 gradlePlugin {
